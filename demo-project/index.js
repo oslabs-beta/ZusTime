1963ca9f6bbd371340b9e2b2e1c995cc980f1783
Jackie Nguyen <jackquelineanguyen@gmail.com>
2022-12-12 19:05:45 -0800

    adding demo project

---
+++ demo-project/index.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { render } from 'react-dom';
+import App from './App.jsx';
+
+createRoot(document.getElementById('root')).render(<App />);
